69722fa427fcdf7f211cd8fd7a8b137d14edf43f
rafael.gorski <rafael.gorski@csgi.com>
2016-04-08 10:05:28 -0300

    installation procedures, readme update, external modules

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,46 @@
+﻿# Rafael Gorski's POwershell Profile to load posh-git and posh-svn
+# Code adapted from http://stackoverflow.com/questions/4500134/posh-git-and-posh-hg-together
+
+function isCurrentDirectoryARepository($type) {
+
+    if ((Test-Path $type) -eq $TRUE) {
+        return $TRUE
+    }
+
+    # Test within parent dirs
+    $checkIn = (Get-Item .).parent
+    while ($checkIn -ne $NULL) {
+        $pathToTest = $checkIn.fullname + '/' + $type;
+        if ((Test-Path $pathToTest) -eq $TRUE) {
+            return $TRUE
+        } else {
+            $checkIn = $checkIn.parent
+        }
+    }
+    return $FALSE
+}
+
+# Posh-Svn and Posh-git prompt on $PWD.Path
+Push-Location 'C:\git\'
+	.\posh-svn\profile.example.ps1
+	.\posh-git\profile.example.ps1
+Pop-Location
+
+function prompt(){
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    Write-Host($pwd) -nonewline
+
+    if (isCurrentDirectoryARepository(".git")) {
+        # Git Prompt
+        $Global:GitStatus = Get-GitStatus
+        Write-GitStatus $GitStatus
+    } elseif (isCurrentDirectoryARepository(".svn")) {
+        # Mercurial Prompt
+        $Global:SvnStatus = Get-SvnStatus
+        Write-SvnStatus $SvnStatus
+    }
+
+    return "> "
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # profile-powershell
 Gorski's Powershell profile script to load posh-git and posh-svn
+
+# How to install it?
+- git clone https://github.com/RafaelGorski/profile-powershell.git
+- cd profile-powershell
+- git submodule update --init
+- cp .\Microsoft.PowerShell_profile.ps1 $profile
+
+# Pre-requisites
+Install Git
+Install Tortoise-Svn
+Install Powershell 2.0+  (Verify your version with $PSVersionTable.PSVersion command)
